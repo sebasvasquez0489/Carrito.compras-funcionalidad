b2cbbf5b69a5b186fc07d8ffa54ac6bcb9435445
Sebastian Vasquez <sebitas0489@gmail.com>
2023-12-11 22:16:27 -0500

    Se crea objeto para extraer el contenido del curso desde el HTML.

--- js/app.js
+++ js/app.js
@@ -9,13 +9,29 @@ cargarEventListeners()
 function cargarEventListeners() {
   // Cuando agregas un curso presionando "Agregar al carrito"
   listaCursos.addEventListener('click', agregarCurso);
-}
+};
 
 // Funciones
 function agregarCurso(e) {
   e.preventDefault(); //Se utiliza para controlar el comportamiento del evento.
 
   if(e.target.classList.contains('agregar-carrito')) {
-    console.log('Presionando en cursos');
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    leerDatosCurso(cursoSeleccionado);
+  };
+};
+
+//Leer contenido del HTML y extraer información.
+function leerDatosCurso(curso) {
+  console.log(curso);
+
+  //Creamos un Objeto con el contenido del curso actual o selecionado
+  const infoCurso = {
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id'),
+    cantidad: 1,
   }
+  console.log(infoCurso);
 }
